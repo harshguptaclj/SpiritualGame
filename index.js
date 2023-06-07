761cf80f132f0746548d9371dc4256176be1b376
Harsh Gupta <96175114+harshguptaclj@users.noreply.github.com>
2023-06-07 14:51:53 +0530

    Added essential files

---
+++ index.js
@@ -0,0 +1,97 @@
+var names=prompt("Please Enter Your Name");
+alert("Hello "+  names+ "\nHare Krishna! \n"+ "Are you Ready? Click OK to start the game!\n" );
+var g1=parseInt(prompt("How much you will rate yourself on\n'CLEANLINESS'\nEnter an integer (0-10)"));
+var p1=parseInt(prompt("How much you will rate yourself on\n'PRIDE'\nEnter an integer (0-10)"));
+var i1=parseInt(prompt("How much you will rate yourself on\n'OVERSLEEP'\nEnter an integer (0-10)"));
+
+var g2=parseInt(prompt("How much you will rate yourself on\n'REGULATION'\nEnter an integer (0-10)"));
+var p2=parseInt(prompt("How much you will rate yourself on\n'LUSTY'\nEnter an integer (0-10)"));
+var i2=parseInt(prompt("How much you will rate yourself on\n'LAZINESS'\nEnter an integer (0-10)"));
+
+var g3=parseInt(prompt("How much you will rate yourself on\n'WISDOM'\nEnter an integer (0-10)"));
+var p3=parseInt(prompt("How much you will rate yourself on\n'GREED'\nEnter an integer (0-10)"));
+var i3=parseInt(prompt("How much you will rate yourself on\n'MADNESS'\nEnter an integer (0-10)"));
+
+var g4=parseInt(prompt("How much you will rate yourself on\n'SIMPLICITY'\nEnter an integer (0-10)"));
+var p4=parseInt(prompt("How much you will rate yourself on\n'OVERAMBITIOUS'\nEnter an integer (0-10)"));
+var i4=parseInt(prompt("How much you will rate yourself on\n'INSECURE'\nEnter an integer (0-10)"));
+
+var g5=parseInt(prompt("How much you will rate yourself on\n'FORGIVENESS'\nEnter an integer (0-10)"));
+var p5=parseInt(prompt("How much you will rate yourself on\n'INTOXICATED'\nEnter an integer (0-10)"));
+var i5=parseInt(prompt("How much you will rate yourself on\n'POSTPONING'\nEnter an integer (0-10)"));
+
+var g6=parseInt(prompt("How much you will rate yourself on\n'THOUGHTFULNESS'\nEnter an integer (0-10)"));
+var p6=parseInt(prompt("How much you will rate yourself on\n'IMPRESS OPPOSITE SEX'\nEnter an integer (0-10)"));
+var i6=parseInt(prompt("How much you will rate yourself on\n'NO THINKING'\nEnter an integer (0-10)"));
+
+var g7=parseInt(prompt("How much you will rate yourself on\n'CONTROLLED MIND'\nEnter an integer (0-10)"));
+var p7=parseInt(prompt("How much you will rate yourself on\n'BOSING TENDENCY'\nEnter an integer (0-10)"));
+var i7=parseInt(prompt("How much you will rate yourself on\n'INDECISIVENESS'\nEnter an integer (0-10)"));
+
+var g8=parseInt(prompt("How much you will rate yourself on\n'SMART DECISION/ADVISE'\nEnter an integer (0-10)"));
+var p8=parseInt(prompt("How much you will rate yourself on\n'CUT THROAT COMPETITION'\nEnter an integer (0-10)"));
+var i8=parseInt(prompt("How much you will rate yourself on\n'SUICIDAL THOUGHTS'\nEnter an integer (0-10)"));
+
+var g9=parseInt(prompt("How much you will rate yourself on\n'DISCIPLINE & OBEDIENCE'\nEnter an integer (0-10)"));
+var p9=parseInt(prompt("How much you will rate yourself on\n'THIRST FOR HONOR & PRESTIGE'\nEnter an integer (0-10)"));
+var i9=parseInt(prompt("How much you will rate yourself on\n'PANICKY/CRY'\nEnter an integer (0-10)"));
+
+var g10=parseInt(prompt("How much you will rate yourself on\n'NOBLE, SAINTLY, GODLY'\nEnter an integer (0-10)"));
+var p10=parseInt(prompt("How much you will rate yourself on\n'TO SEE AND SHOW GLAMOUR'\nEnter an integer (0-10)"));
+var i10=parseInt(prompt("How much you will rate yourself on\n'BEWILDERED'\nEnter an integer (0-10)"));
+
+var g11=parseInt(prompt("How much you will rate yourself on\n'COMPASSIONATE TO ALL'\nEnter an integer (0-10)"));
+var p11=parseInt(prompt("How much you will rate yourself on\n'ENVIOUS & MALICIOUS'\nEnter an integer (0-10)"));
+var i11=parseInt(prompt("How much you will rate yourself on\n'INTOXICATED'\nEnter an integer (0-10)"));
+
+var g12=parseInt(prompt("How much you will rate yourself on\n'CREATIVE THINKING'\nEnter an integer (0-10)"));
+var p12=parseInt(prompt("How much you will rate yourself on\n'PARTIAL THINKING'\nEnter an integer (0-10)"));
+var i12=parseInt(prompt("How much you will rate yourself on\n'DEPRESSION'\nEnter an integer (0-10)"));
+
+var g13=parseInt(prompt("How much you will rate yourself on\n'CONTROLLED SENSES'\nEnter an integer (0-10)"));
+var p13=parseInt(prompt("How much you will rate yourself on\n'SNAP DECISIONS'\nEnter an integer (0-10)"));
+var i13=parseInt(prompt("How much you will rate yourself on\n'CONFUSED'\nEnter an integer (0-10)"));
+
+var g14=parseInt(prompt("How much you will rate yourself on\n'REALISED WISDOM'\nEnter an integer (0-10)"));
+var p14=parseInt(prompt("How much you will rate yourself on\n'ANGRY'\nEnter an integer (0-10)"));
+var i14=parseInt(prompt("How much you will rate yourself on\n'HOPELESS'\nEnter an integer (0-10)"));
+
+var g15=parseInt(prompt("How much you will rate yourself on\n'CALM,CLEAR'\nEnter an integer (0-10)"));
+var p15=parseInt(prompt("How much you will rate yourself on\n'GENEROUS'\nEnter an integer (0-10)"));
+var i15=parseInt(prompt("How much you will rate yourself on\n'FOOLISH'\nEnter an integer (0-10)"));
+
+var g16=parseInt(prompt("How much you will rate yourself on\n'TOLERANCE'\nEnter an integer (0-10)"));
+var p16=parseInt(prompt("How much you will rate yourself on\n'INDIVIDUAL PLAYER'\nEnter an integer (0-10)"));
+var i16=parseInt(prompt("How much you will rate yourself on\n'FEARFUL'\nEnter an integer (0-10)"));
+
+var g17=parseInt(prompt("How much you will rate yourself on\n'FRIENDLY'\nEnter an integer (0-10)"));
+var p17=parseInt(prompt("How much you will rate yourself on\n'RESTLESS MIND & SENSES'\nEnter an integer (0-10)"));
+var i17=parseInt(prompt("How much you will rate yourself on\n'DULL'\nEnter an integer (0-10)"));
+
+var g18=parseInt(prompt("How much you will rate yourself on\n'SUBMISSIVENESS'\nEnter an integer (0-10)"));
+var p18=parseInt(prompt("How much you will rate yourself on\n'ATTACHMENT & DESIRE'\nEnter an integer (0-10)"));
+var i18=parseInt(prompt("How much you will rate yourself on\n'INATTENTIVE'\nEnter an integer (0-10)"));
+
+var goodness=(g1+g2+g3+g4+g5+g6+g7+g8+g9+g10+g11+g12+g13+g14+g15+g16+g17+g18)/18;
+var passion=(p1+p2+p3+p4+p5+p6+p7+p8+p9+p10+p11+p12+p13+p14+p15+p16+p17+p18)/18;
+var ignorance=(i1+i2+i3+i4+i5+i6+i7+i8+i9+i10+i11+i12+i13+i14+i15+i16+i17+i18)/18;
+
+alert("Your Goodness % is "+(goodness*10) + "\nYour Passion % is "+(passion*10) + "\nYour Ignorance % is "+(ignorance*10) + "\nClick OK to know more!");
+if(goodness>passion && goodness>ignorance)
+{
+    alert("Dear "+names+" as your goodness level is "+(goodness*10)+" %, which is highest, it shows you are a spiritual person.\n Try to decrease the % of ignorance ("+(ignorance*10)+"%) and passion ("+  (passion*10)+") by increasing your spiritual activities.\nHare Krishna!");
+}
+else if(passion>goodness && passion>ignorance)
+{
+    alert("Dear "+names+" as your passion level is "+(passion*10)+" %, which is highest, it shows you are a passionate person.\n Try to increase the % of goodness (" +(goodness*10) + "%) and decrease the % of ignorance ("+(ignorance*10)+"%) by doing some spiritual activities.\nHare Krishna!");
+}
+else if(ignorance>passion && ignorance>goodness)
+{
+    alert("Dear "+names+" as your ignorance level is "+(ignorance*10)+" %, which is highest, it shows you are a ignorant person.\nYou are advised to do some spiritual activities to increase the % of goodness and decrease the % of passion and ignorance.\nHare Krishna!");
+}
+else
+{
+    alert("Dear "+names+" your are advised to engage in spiritual activities to increase the % of goodness in yourself.");
+}
+alert("Thanks For Playing the Game\nDo give your valuable Suggestion and Feedback at\nharshguptaclj@gmail.com");
+
